aa0c2bc572c068a2a31e7e0ea2f9547a5e281b78
haungxi <1842196429@qq.com>
2023-09-04 08:44:01 +0800

    修复图片logo路径问题

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -13,7 +13,7 @@ export default hopeTheme({
 
   iconAssets: "fontawesome-with-brands",
 
-  logo: "/logo.jpg",
+  logo: "./public/logo.jpg",
   // 仓库链接
   repo: "https://github.com/huangxi-184/hx-docs",
 
