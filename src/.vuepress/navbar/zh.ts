1802ebec313f60b271faf1ffe83a6bb38adbf0f5
haungxi <1842196429@qq.com>
2024-06-26 15:12:46 +0800

    ✨ feat(目录修订): 修改目录结构

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -3,73 +3,9 @@ import { navbar } from 'vuepress-theme-hope';
 export const zhNavbar = navbar([
   '/',
   {
-    text: 'Web',
+    text: '大前端',
     prefix: '/web',
-    children: [
-      {
-        text: '开发常识',
-        prefix: '/axiom/',
-        activeMatch: '^/web/axiom/$',
-        children: ['git', 'Markdown', 'naming-conventions']
-      },
-      {
-        text: 'CSS',
-        prefix: '/css/',
-        children: ['grid', 'rem', 'tailwindCSS']
-      },
-      {
-        text: 'js',
-        prefix: '/JS/',
-        children: ['es7', 'es8', 'es9', 'es10', 'es11', 'es12', 'promise']
-      },
-      {
-        text: 'Project',
-        prefix: '/Project/',
-
-        children: [
-          'funCode/复杂表格',
-          'funCode/获得摄像头的数据流',
-          'funCode/图片转CSS',
-          'tinymce/index',
-          'vbenAdmin/i18n',
-          'vbenAdmin/tableCom',
-          'vbenAdmin/tips',
-          '数据大屏',
-          'chrome'
-        ]
-      },
-      {
-        text: 'TypeScript',
-        prefix: '/TS/',
-        children: ['ts入门']
-      },
-      {
-        text: 'Vue',
-        prefix: '/Vue/',
-        children: ['组件通信方式', 'HTTP状态码', 'NuxtJs', 'vue总结']
-      },
-      {
-        text: 'React',
-        prefix: '/React/',
-        children: ['描述UI']
-      },
-      {
-        text: '面经',
-        prefix: '/interview/',
-        children: ['01', '02']
-      }
-    ]
-  },
-  {
-    text: '客户端',
-    prefix: '/client/',
-    children: [
-      {
-        text: 'Android',
-        prefix: 'Android/',
-        children: ['androidbase']
-      }
-    ]
+    children: ['/axiom', '/Css/', '/JavaScript/', '/Project', '/TypeScript', '/Vue', '/React', '/面经', '/Android']
   },
   {
     text: '服务端',
@@ -119,12 +55,12 @@ export const zhNavbar = navbar([
     children: ['PMS开通账号']
   },
   {
-    text: 'Vercel链接',
+    text: 'Vercel项目',
     children: [
       {
         text: 'Next-Notes',
         link: 'https://next-react-notes-six.vercel.app/'
-      },
+      }
     ]
   },
   {
