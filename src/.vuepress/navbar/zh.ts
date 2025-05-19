d4c51d3ac9dcbbef138fa5e57d09963cf3b799e6
huangxi <huangxi0226@163.com>
2025-05-19 18:05:28 +0800

    ✨ feat(新笔记):添加新笔记

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -58,7 +58,7 @@ export const zhNavbar = navbar([
   {
     text: '备忘录',
     prefix: '/others/',
-    children: ['PMS开通账号', '保质期计算器', 'bilibiliBanner', 'NcmToMp3']
+    children: ['PMS开通账号', '保质期计算器', 'bilibiliBanner', 'NcmToMp3', '大屏展示']
   },
   {
     text: 'Vercel项目',
